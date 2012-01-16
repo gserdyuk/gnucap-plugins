479645617237784df3066f86a31164c26c11c492
gserdyuk <gserdyuk@mail.ru>
2012-01-16 03:04:54 +0200

    noise list add/ remove works

--- noise/s_noise.cc
+++ noise/s_noise.cc
@@ -1,6 +1,8 @@
 /*$Id
  * noise analysis top
+ *   
  */
+
 //
 /*
 #include "u_sim_data.h"
@@ -9,13 +11,12 @@
 #include "u_prblst.h"
 #include "s__.h"
 #include "s_ac.h"
-
-#include "e_node.h" //GS - needed by setup (nodes)
-#include "u_nodemap.h"
 */
 
-#include "e_node.h" //GS - needed by setup (nodes)
+#include "e_node.h"     // GS - needed by setup (nodes)
 #include "u_nodemap.h"
+#include "l_lib.h"
+
 
 #include "s_ac.cc"
 
@@ -25,8 +26,11 @@ namespace {
 
 class NOISE : public AC {
 
-enum NoiseRelType  {STD=1, RELVAL=2, RELVOL=3};  /* how calculate gain? as STD: Vout, RELVAL:Vout/source value or RELVOL: Vout/Vin*/ 
-enum NoiseOutType  {POW=1, VOLT=2};         /* how to output noise? as v^2/hz (POW) or as volt / sqrt(hz) VOLT) */
+/* how calculate gain? as STD: Vout, RELVAL:Vout/source value or RELVOL: Vout/Vin*/ 
+enum NoiseRelType  {STD_NoiseType=1, RELVAL_NoiseType=2, RELVOL_NoiseType=3};  
+
+/* how to output noise? as v^2/hz (POW) or as volt / sqrt(hz) VOLT) */
+enum NoiseOutType  {POW_NoiseOutType=1, VOLT_NoiseOutType=2};         
 
 public:
   void	do_it(CS&, CARD_LIST*);
@@ -45,8 +49,6 @@ protected:
   virtual void	setup(CS&);
 private:
 
-  //PARAMETER<int> _onode1; 		// node where to measure noise
-  //PARAMETER<int> _onode2;       // reference node, default =0
   std::string _onode1_name; 		// node where to measure noise
   std::string _onode2_name;         // reference node, default =0
 
@@ -60,13 +62,8 @@ private:
   int _onode2_map;			// mapped (internal numbering) _onode1 and _onode2
   int _inode1_map;
   int _inode2_map;			// mapped (internul numbering) nodes of source "_source_name"
-  
-  
+    
   // tmp
-public:
-  void alloc_vectors();
-  void unalloc_vectors();
-  void init();
 public:
   // debug and dev functions
   void outport_source();  
@@ -133,27 +130,6 @@ double Integral::integrateStep(double x, double y){
 	}
 
 /*--------------------------------------------------------------------------*/
-/*--------------------------------------------------------------------------
-void NOISE::alloc_vectors(){
-
-	AC::alloc_vectors();	
-
-	assert(!noise_ac);
-	noise_ac = new COMPLEX[(::status.total_nodes+1)];
-}*/
-/*--------------------------------------------------------------------------
-void NOISE::unalloc_vectors(){
-
-	AC::unalloc_vectors();
-	
-	assert(noise_ac);
-	delete [] noise_ac;
-	noise_ac=NULL;
-}*/
-/*--------------------------------------------------------------------------
-void NOISE::init(){
-	AC::init();
-} */
 /*--------------------------------------------------------------------------*/
 void NOISE::outport_source(){  
 	//fills noise_ac at output nodes
@@ -184,7 +160,6 @@ void NOISE::do_it(CS& Cmd, CARD_LIST* Scope)
   _sim->set_command_noise();     
   reset_timers();
   //::status.ac.reset().start();		// put proper call todo - same here
- 
   _sim->init();				
   _sim->alloc_vectors();		        // alloc vectors for noise ; AC::alloc_vectors();
   assert(!noise_ac);
@@ -201,7 +176,7 @@ void NOISE::do_it(CS& Cmd, CARD_LIST* Scope)
   case rBATCH:		untested();
   case rINTERACTIVE:untested();     // todo test yet
   case rSCRIPT:		sweep();		break;
-  case rPRESET:		break; // nothing
+  case rPRESET:		break;          // nothing
   }
   _sim->_acx.unallocate();
 
@@ -210,8 +185,8 @@ void NOISE::do_it(CS& Cmd, CARD_LIST* Scope)
   delete [] noise_ac;
   noise_ac=NULL;
 
-  //::status.ac.stop();		// todo
-  //::status.total.stop();    // todo
+  //::status.ac.stop();		    // todo
+  //::status.total.stop();      // todo
 
 }
 /*--------------------------------------------------------------------------*/
@@ -222,6 +197,7 @@ void NOISE::setup(CS& Cmd)
   // GS: read _out__node, _out_ref_node and _source_name here  
   // format:  v(1[,3]) Vin .........
   //int node1,node2;
+  
   std::string name1,name2;
   
   Cmd>>name1;								// got V
@@ -235,12 +211,10 @@ void NOISE::setup(CS& Cmd)
   Cmd>>name2;								// got SourceName
 	  
   // finally assign what did we find
-  //_onode1=node1;
-  //_onode2=node2;
   _source_name=name2;
   
-  _noise_rel_t=STD;
-  _noise_out_t=POW;
+  _noise_rel_t=STD_NoiseType;       // others are possible but not fully implemented
+  _noise_out_t=POW_NoiseOutType;  
   
   AC::setup(Cmd);
   _out.setformat(ftos_EXP);  // _out created in AC::setup(); constant is from l__lib.h
@@ -248,39 +222,24 @@ void NOISE::setup(CS& Cmd)
   // now extract information regarding source (where to refer noise to)
   // A extract nodes and use them to have Vin voltage
   CARD_LIST::iterator e= CARD_LIST::card_list.find_(_source_name);
-  // (*e)->print("####");  // TMP output
 
-   // TODO! check if e!=0 and raise exception if e==0 (it means that no device named "_source_name")
-   // TODO! check if (*e) is a pointer to valid source. if not - raise exception.
+  // TODO! check if e!=0 and raise exception if e==0 (it means that no device named "_source_name")
+  // TODO! check if (*e) is a pointer to valid source. if not - raise exception.
    
-   assert(e != CARD_LIST::card_list.end());   
-   _inode1_map=(*e)->n_(0).m_();	//_n[OUT1].m_();
-   _inode2_map=(*e)->n_(1).m_();	//_n[OUT2].m_();
+  assert(e != CARD_LIST::card_list.end());   
+  _inode1_map=(*e)->n_(0).m_();	//_n[OUT1].m_();
+  _inode2_map=(*e)->n_(1).m_();	//_n[OUT2].m_();
 
-	assert(_inode1_map!=_inode2_map);   // otherwize K in solve() will be zero
+  assert(_inode1_map!=_inode2_map);   // otherwize K in solve() will be zero
 	
-/* //mapped nodes 
-   assert(SIM::nm);
-   for(int kk=0;  kk< ::status.total_nodes+1; kk++){
-  	_out<< " kk="<<kk << " nm[kk]="<<SIM::nm[kk]<<	"\n";
-	};
-*/
-	// TODO!: check if nodes _onode1 and _onode2 valid and rize exception if those nodes are not valid 
-   //_onode1_map=_sim->_nm[int(_onode1)];
-   //_onode2_map=_sim->_nm[int(_onode2)];
-
-  //_out<<" _onode1_name ="<<_onode1_name<< " _onode2_name    ="<<_onode2_name    <<'\n';
+  // TODO!: check if nodes _onode1 and _onode2 valid and rize exception if those nodes are not valid 
+  //_onode1_map=_sim->_nm[int(_onode1)];
+  //_onode2_map=_sim->_nm[int(_onode2)];
    
-    // handle errors - todo yet
-   _onode1_map=CARD_LIST::card_list.nodes()->operator[](_onode1_name)->m_();
-   _onode2_map=CARD_LIST::card_list.nodes()->operator[](_onode2_name)->m_();
-   
-   
-   // _onode1_map=2;// check  look at _nm, study it
-      
-  //_out<<" _inode1_map  ="<<_inode1_map << " _inode2_map="<<_inode2_map<<'\n';
-  //_out<<" _onode1_map  ="<<_onode1_map << " _onode2_map="<<_onode2_map<<'\n';
-  
+  // handle errors - todo yet
+  _onode1_map=CARD_LIST::card_list.nodes()->operator[](_onode1_name)->m_();
+  _onode2_map=CARD_LIST::card_list.nodes()->operator[](_onode2_name)->m_();
+     
 }
 /*--------------------------------------------------------------------------*/
 void NOISE::solve()
@@ -295,11 +254,11 @@ void NOISE::solve()
   CARD_LIST::card_list.do_ac();
   CARD_LIST::card_list.ac_load();
 
-  outport_source(); //  fill noise_ac
+  outport_source();                     //  fill noise_ac
   
-  //_sim->_acx.printall(); 
-  //print_ac();
-  //print_noise_ac();
+  // _sim->_acx.printall(); 
+  // print_ac();
+  // print_noise_ac();
   
   //::status.load.stop();				//todo
 
@@ -314,8 +273,8 @@ void NOISE::solve()
   ::status.back.start();
   _sim->_acx.fbsub(_sim->_ac);			// here are sources of AC. for noise use adjoint system
   ::status.back.stop();
-  //std::cout<<"ac solved\n";  
-  //print_ac();
+  // std::cout<<"ac solved\n";  
+  // print_ac();
   
   // use transposed acx and solve adjoint system
   //::status.back.start();  		//todo - fix call
@@ -325,55 +284,48 @@ void NOISE::solve()
   //print_noise_ac();
   
   // TODO HERE  multiply noise_ac onto matrix, get initial result               !!!!
-  // this will alow to check is solution is correct
+  // this will allow to check if solution is correct
   
   
   // calculate noise 
   // put formulas and reference here	//todo
   double noisepow=0;
   CARD_LIST::card_list.do_noise(noisepow, noise_ac);   // todo! - here - implement noise in different models
-  
-  /*	{
-	double noisepow_tmp=0;
-	// R1
-	double i_noise_R1=sqrt(4*1.3806503e-23*300.15/50e3);  //r1 1 2  (mapped 3 2)
-	COMPLEX V_R1=noise_ac[3]-noise_ac[2];
-	double noisepow_R1=abs(V_R1*i_noise_R1);
-	noisepow_R1 *= noisepow_R1;
-  	noisepow_tmp+=noisepow_R1 ;	 
-	_out<<"noisepow_R1="<<noisepow_R1<<std::endl;  
-  	}	
-  */
-  	
+                        /*
+                        implemented:
+                        CARD        - virtual, returns zero
+                        R           - resistor
+                        cardlist    - set of devices
+                        subckt      - SUBCKT, uses cardlist
+                        dev_spice   - all spice models 
+                        to update: "grep -I do_noise * "
+                        */	
   double k;
-  if (_noise_rel_t==STD){
-    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map]) / 1.  );  	// k- transfer out node to input voltage, assume input=1   
+  if (_noise_rel_t==STD_NoiseType){           // -- onl;y this one is fully implemneted
+    // k- transfer out node to input voltage, assume input=1   
+    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map]) / 1.  );  	
     }
-  else if (_noise_rel_t==RELVOL){
-    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map])/(_sim->_ac[_inode1_map]-_sim->_ac[_inode2_map])  );  	// k- transfer out node to input voltage   
+  else if (_noise_rel_t==RELVOL_NoiseType){
+    // k- transfer out node to input voltage   
+    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map])/(_sim->_ac[_inode1_map]-_sim->_ac[_inode2_map])  );  	
     }
-  else if (  _noise_rel_t==RELVAL){
+  else if (  _noise_rel_t==RELVAL_NoiseType){
     double inval =1;  // FIXIT - read input value from device
-    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map]) / inval  );  	// k- transfer out node to input voltage , need to read inval from device
+    // k- transfer out node to input voltage , need to read inval from device
+    k=abs( (_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map]) / inval  );  	
     }
   else {unreachable();}
-/*
-  std::cout<<" vout= "<<_sim->_ac[_onode1_map]-_sim->_ac[_onode2_map]<<'\n';
-  std::cout<<" vin=  "<<_sim->_ac[_inode1_map]-_sim->_ac[_inode2_map]<<'\n';
-  std::cout<<" k=    "<<k<<'\n';
-*/
+
   inoise_density=noisepow/k/k;
   onoise_density=noisepow;
-  return;
 
+  return;
 } 
 
 /*--------------------------------------------------------------------------*/
 void NOISE::sweep()
 {
-  //head(start(), stop(), "Freq"); 
-  // todo - fast and dirty printing
-  _out<<"#Freq"<<'\t'<<"inoise_density"<<'\t'<<"onoise_density"<<"\n";
+  head(start(), stop(), "Freq"); // SIC! Allocates WAVE
   
   first();
 
@@ -392,23 +344,20 @@ void NOISE::sweep()
     _sim->_inoise=inoise_density;
     _sim->_onoise=onoise_density;
     outdata(_sim->_freq);
-	//std::cout<<_sim->_freq<<'\t'<<inoise_density<<'\t'<<onoise_density<<"\n";;
     
 	// integrate noise over band
-	inoiz.integrateTrapez(_sim->_freq,inoise_density);
-	onoiz.integrateTrapez(_sim->_freq,onoise_density);
-	//inoiz.integrateStep(_sim->_freq,inoise_density);
-	//onoiz.integrateStep(_sim->_freq,onoise_density);
+	inoiz.integrateTrapez(_sim->_freq,inoise_density);   	//inoiz.integrateStep(_sim->_freq,inoise_density);
+	onoiz.integrateTrapez(_sim->_freq,onoise_density);      //onoiz.integrateStep(_sim->_freq,onoise_density);
 
   } while (next());
   
   _sim->_inoise_tot=inoiz.value();
   _sim->_onoise_tot=onoiz.value();
-  _out<<"\n";
 
-  _out<<"#index"<<'\t'<<"inoise_total"<<'\t'<<"onoise_total"<<"\n";
-  _out<<0<<'\t'<<inoiz.value()<<'\t'<<onoiz.value()<<"\n\n";
-
+   command("measure inoise_total=eval("+std::string(ftos(inoiz.value(),0,10,ftos_EXP ))+")", 
+            &CARD_LIST::card_list); 
+   command("measure onoise_total=eval("+std::string(ftos(onoiz.value(),0,10,ftos_EXP ))+")", 
+            &CARD_LIST::card_list);
 } 
 
 /*--------------------------------------------------------------------------*/
